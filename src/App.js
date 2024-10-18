34a5e23f449bdcbb448f5c6b65b6fbf4a0747b66
Zack Vera <qphys50@gmail.com>
2024-10-18 15:01:01 -0700

    Setup BookCreate functionality and added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-    const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    return <div>App</div>;
+  const createBook = (title) => {
+    console.log('Need to add book with:', title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
