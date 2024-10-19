3fac2172a013eddfbe58311c4d99b2c13f3c0852
Zack Vera <qphys50@gmail.com>
2024-10-18 17:47:48 -0700

    Actually showing the books in card-type elements.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
