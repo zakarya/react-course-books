5d4223713b86f30e9d6885f7a954e741286f8c13
Zack Vera <qphys50@gmail.com>
2024-10-22 18:30:09 -0700

    Setting up the Context providers and learning how to change context.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,23 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/book";
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click!</button>
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "./context/book";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider value={10}>
+    <App />
+  </Provider>,
+);
