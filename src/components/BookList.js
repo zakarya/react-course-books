6cfb38b39df9487bebdd52838d75483af413f5da
Zack Vera <qphys50@gmail.com>
2024-10-23 15:10:47 -0700

    Refactored the codebase to use Context throughout.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,15 @@
-import BookShow from "./BookShow";
 import { useContext } from "react";
 import BooksContext from "../context/book";
+import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
+function BookList() {
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click!</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,25 +1,24 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/book";
 import BookEdit from "./BookEdit";
-import bookEdit from "./BookEdit";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>;
-
   if (showEdit) {
     content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
